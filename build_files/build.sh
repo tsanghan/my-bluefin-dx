39f4a98556aa9c5b093d4dbb96929d68786e3338
WONG Tsang Han <tsanghan@gmail.com>
2026-05-09 20:53:50 +0800

    15th commit

--- build_files/build.sh
+++ build_files/build.sh
@@ -17,6 +17,12 @@ echo 'install_items+=" /etc/modprobe.d/thinkfan.conf "' | sudo tee /etc/dracut.c
 # ostree container commit
 systemctl enable --now thinkfan
 
+KERNEL_SUFFIX=""
+QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(|'"$KERNEL_SUFFIX"'-)(\d+\.\d+\.\d+)' | sed -E 's/kernel-(|'"$KERNEL_SUFFIX"'-)//')"
+export DRACUT_NO_XATTR=1
+/usr/bin/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible -v --add ostree -f "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
+chmod 0600 "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
+
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
